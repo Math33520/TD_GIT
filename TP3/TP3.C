2f4947c7ab6402bbf3ae016a60a6a0b779a29438
Mathieu <mathieu.bertron@outlook.fr>
2025-11-17 15:06:00 +0100

    étape 4

--- TP3/TP3.C
+++ TP3/TP3.C
@@ -1,27 +1,57 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main() {
-    int n ;
+    int n;
     int width;
     int max_height;
     char img[100][100];
-    printf("Saisir une taille  n (5<=x<=10):");
-    scanf("%d",&n);
-    if (n<4 || n>11){
-        printf("La taille entrez est pas dans l'intervalle demander ");
+    int mid;
+
+    printf("Saisir une taille n (5<=n<=10): ");
+    scanf("%d", &n);
+
+    if (n < 4 || n > 11) {
+        printf("La taille entree n'est pas dans l'intervalle demande.\n");
         scanf("%d",&n);
     }
-    else{
-        width = n *n ;
-        max_height= 3*n-1;
 
-    for(int i=0;i<=width;i++){
-        for (int j=0;j<=max_height;i++){
-            img[i][j]= ' ';
-            printf("%c",img[i][j]);
+    width = n * n;
+    max_height = 3 * n - 1;
+
+    for (int i = 0; i < max_height; i++) {
+        for (int j = 0; j < width; j++) {
+            img[i][j] = ' ';
         }
+    }
 
+    mid = width / 2;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < width; j++) {
+            int k = j - mid;
+            if (k == -i || k == i) {
+                img[i][j] = 'A';
+            } else if (k > -i && k < i) {
+                img[i][j] = 'S';
+            }
+        }
     }
+
+    int left = mid - (n - 1);
+    int right = mid + (n - 1);
+
+    for (int j = 0; j < width; j++) {
+        if (j < left || j > right) {
+            img[n - 1][j] = '_';
+        }
     }
-    
+
+    for (int i = 0; i < max_height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%c", img[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
