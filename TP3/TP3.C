3e97639870a050c94d0019a6f1badaf19efbd96a
Mathieu <mathieu.bertron@outlook.fr>
2025-11-17 13:32:10 +0100

    étape1 modif

--- TP3/TP3.C
+++ TP3/TP3.C
@@ -4,16 +4,23 @@ int main() {
     int n ;
     int width;
     int max_height;
-    char img;
+    char img[100][100];
     printf("Saisir une taille  n (5<=x<=10)");
-    scanf("%d",n);
-    if (n>11 & n <4){
-        printf("La taille netrez est pas dans l'intervalle demander ");
+    scanf("%d",&n);
+    if (n<4 || n>11){
+        printf("La taille entrez est pas dans l'intervalle demander ");
     }
     else{
         width = n *n ;
         max_height= 3*n-1;
 
+    for(int i=0;i<=n;i++){
+        for (int j=0;j<=max_height;i++){
+            img[i][j]= ' ';
+            printf("%s",img[i][j]);
+        }
+
     }
+
 
 }
