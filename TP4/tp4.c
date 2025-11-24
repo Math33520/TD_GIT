0caaefd0b75e4bc7d96e973d2e36c0ea1df7c46c
Mathieu <mathieu.bertron@outlook.fr>
2025-11-24 13:23:14 +0100

    étape3

--- TP4/tp4.c
+++ TP4/tp4.c
@@ -16,15 +16,31 @@ void affichemenu(){
 int lirechoix()
 {
     int choix;
-    printf("Donnez votre choix parmis tout les nombres");
+    printf("Donnez votre choix parmis tout les nombresc : ");
     scanf("%d",&choix);
     return choix;
+}
+    int saisinombreeleves()
+{
+    int nbeleves;
+    printf("saisir le nombre d'eleves de (1 a 30) : ");
+    scanf("%d",&nbeleves);
+    if (nbeleves<1 || nbeleves>30)
+    {
+        printf("\nValeur invalide ");
+        saisinombreeleves();
+    }
+    else{
+        return nbeleves;
+    }
+
 }
 int main()
 {
     affichemenu();
     int variable = lirechoix();
-    printf("\n Vous avez choisi l'option :%d\n",variable);
-
+    int valeur = saisinombreeleves();
+    printf("\n Vous avez choisi l'option :%d",variable);
+    printf("\n nombre d'eleves :%d",valeur);
     return 0;
 }
