ff94c4b6083b86b5cf2a91faf0c9d9bd8274e1cd
Mathieu <mathieu.bertron@outlook.fr>
2025-11-28 08:34:09 +0100

    ex2.fini

--- TP4/ex2.c
+++ TP4/ex2.c
@@ -7,7 +7,57 @@ void afficherMenu()
     printf("2 - Quitter\n");
 }
 
-int main ()
-{
+int saisir_montant() {
+    int montant;
+    printf("Montant a retirer : ");
+    scanf("%d", &montant);
+    return montant;
+}
 
+int montant_valide(int montant) {
+    if (montant < 5 || montant > 1000 || montant % 5 != 0) {
+        printf("Erreur : montant invalide.\n");
+        printf("Le montant doit etre entre 5 euros et 1000 euros, divisible par 5.\n");
+        return 0;
+    }
+    return 1;
+}
+
+void calcul_distribution(int montant) {
+
+    int b50 = montant / 50;
+    montant = montant % 50;
+
+    int b20 = montant / 20;
+    montant = montant % 20;
+
+    int b10 = montant / 10;
+    montant = montant % 10;
+
+    int b5  = montant / 5;
+
+    printf("Billets distribues :\n");
+    if (b50 > 0) printf("%d billets de 50 euros\n", b50);
+    if (b20 > 0) printf("%d billets de 20 euros\n", b20);
+    if (b10 > 0) printf("%d billets de 10 euros\n", b10);
+    if (b5  > 0) printf("%d billets de 5 euros\n",  b5);
+}
+
+int main() {
+    int choix;
+    int montant;
+
+    afficherMenu();
+
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+
+    if (choix == 1) {
+        montant = saisir_montant();
+
+        if (montant_valide(montant)) {
+            calcul_distribution(montant);
+        }
+    }
+    return 0;
 }
